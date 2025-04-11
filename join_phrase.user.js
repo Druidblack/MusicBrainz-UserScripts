6a6de9af34ab1669e581739789edf0659041ba14
Aerozol <aerozol1@gmail.com>
2025-04-11 12:47:21 +1200

    Update join_phrase.user.js
    Support beta.musicbrainz.org
    + mb.eu while I'm at it

--- join_phrase.user.js
+++ join_phrase.user.js
@@ -4,7 +4,8 @@
 // @version      2025.0.11
 // @author       Druidblack
 // @description  Automatically fills in the join-phrase fields for performing credits on the Album and Tracklist tabs.
-// @match        https://musicbrainz.org/*
+// @match        *://*.musicbrainz.org/*
+// @match        *://*.musicbrainz.eu/*
 // @icon         https://musicbrainz.org/favicon.ico
 // @grant        none
 // @downloadURL  https://github.com/Druidblack/MusicBrainz-UserScripts/raw/main/join_phrase.user.js
